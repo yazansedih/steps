74c7b8cb351844c86166f9daf14085a52e7cf214
yazansedih <yazansedih@gmail.com>
2024-10-17 11:40:15 +0300

    add more hooks

--- OneDrive/Documents/GitHub/usepopcorn/src/components/StarRating.js
+++ OneDrive/Documents/GitHub/usepopcorn/src/components/StarRating.js
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
 
-const containerStyte = {
+const containerStyle = {
   display: "flex",
-  alignlterns: "center",
+  alignItems: "center",
   gap: "16px",
 };
 
@@ -12,24 +12,22 @@ const starContainerStyle = {
 };
 
 StarRating.propTypes = {
-  maxRating: PropTypes.number.isRequired,
+  maxRating: PropTypes.number,
   defaultRating: PropTypes.number,
   color: PropTypes.string,
   size: PropTypes.number,
-  messages: PropTypes.arrayOf(
-    PropTypes.shape({ value: PropTypes.number, text: PropTypes.string })
-  ),
+  messages: PropTypes.array,
   className: PropTypes.string,
   onSetRating: PropTypes.func,
 };
 
 export default function StarRating({
-  defaultRating = 0,
+  maxRating = 5,
   color = "#fcc419",
   size = 48,
-  messages = [],
   className = "",
-  maxRating = 5,
+  messages = [],
+  defaultRating = 0,
   onSetRating,
 }) {
   const [rating, setRating] = useState(defaultRating);
@@ -37,18 +35,18 @@ export default function StarRating({
 
   function handleRating(rating) {
     setRating(rating);
-    // onSetRating(rating);
+    onSetRating(rating);
   }
 
   const textStyle = {
-    // lineHeight: "1",
+    lineHeight: "1",
     margin: "0",
     color,
     fontSize: `${size / 1.5}px`,
   };
 
   return (
-    <div style={containerStyte} className={className}>
+    <div style={containerStyle} className={className}>
       <div style={starContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
@@ -71,7 +69,7 @@ export default function StarRating({
   );
 }
 
-function Star({ full, onRate, onHoverIn, onHoverOut, color, size }) {
+function Star({ onRate, full, onHoverIn, onHoverOut, color, size }) {
   const starStyle = {
     width: `${size}px`,
     height: `${size}px`,
@@ -86,8 +84,6 @@ function Star({ full, onRate, onHoverIn, onHoverOut, color, size }) {
       onClick={onRate}
       onMouseEnter={onHoverIn}
       onMouseLeave={onHoverOut}
-      color={color}
-      size={size}
     >
       {full ? (
         <svg
@@ -116,3 +112,24 @@ function Star({ full, onRate, onHoverIn, onHoverOut, color, size }) {
     </span>
   );
 }
+
+/*
+FULL STAR
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 20 20"
+  fill="#000"
+  stroke="#000"
+>
+  <path
+    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+  />
+</svg>
+
+
+EMPTY STAR
+
+
+
+*/
